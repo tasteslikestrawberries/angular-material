c5f9a1c620895afd84b8c0b11c8ab1c44c5b3e97
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-01-27 10:19:44 +0100

    working on closing the sidebar when link is clicked

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { ThemeService } from 'src/app/services/theme.service';
-
+import { MatSidenav } from '@angular/material/sidenav';
+import { tap } from 'rxjs';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -12,6 +13,8 @@ import { ThemeService } from 'src/app/services/theme.service';
 export class NavigationComponent {
   isDarkTheme?: Observable<boolean>;
   isSunIcon = false;
+
+  @ViewChild('drawer', {static: true}) matDrawer?: MatSidenav;
 
   constructor(private breakpointObserver: BreakpointObserver, private themeService: ThemeService) { }
 
@@ -25,5 +28,14 @@ export class NavigationComponent {
       map(result => result.matches),
       shareReplay()
     );
+
+    closeSidebar() {
+      const isHandset= this.breakpointObserver.isMatched(
+        "(max-width: 768px)"
+      );
+      if (isHandset) {
+       this.matDrawer?.close();
+     }
+   }
 
 }

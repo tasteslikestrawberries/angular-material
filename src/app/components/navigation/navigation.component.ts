2e8345dca418fe311a44caabfb05fb6afb0b9d30
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-01-29 11:45:57 +0100

    corrected toggle behavior depending on drawer mode

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -8,15 +8,18 @@ import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
   isSunIcon = false;
 
-  @ViewChild('drawer', {static: true}) matDrawer?: MatSidenav;
+  @ViewChild('drawer', { static: true }) matDrawer?: MatSidenav;
 
-  constructor(private breakpointObserver: BreakpointObserver, private themeService: ThemeService) { }
-  
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private themeService: ThemeService
+  ) {}
+
   /*isHandset$: Observable<boolean> = this.breakpointObserver.observe('(max-width: 720px)')
   //720 is the breakpoint-on 721px the sidebar (drawer) is opened by default
   .pipe(
@@ -24,15 +27,15 @@ export class NavigationComponent {
       shareReplay(1),
   );*/
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset) //check official breakpoints and orientations
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset) //check official breakpoints and orientations
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
   toggleDarkTheme(isChecked: boolean) {
     this.themeService.setDarkTheme(isChecked);
-    this.isSunIcon = !this.isSunIcon ;
+    this.isSunIcon = !this.isSunIcon;
   }
-
 }

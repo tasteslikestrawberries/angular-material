b696616f265ed18bac29ca3a290d20f306be80cb
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-01-31 15:51:15 +0100

    implemented mat sort

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -4,6 +4,7 @@ import { UserService } from 'src/app/services/user.service';
 import { catchError, mapTo, Observable, of, startWith, tap } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-list',
@@ -15,11 +16,17 @@ export class ListComponent implements OnInit {
   isLoading$?: Observable<boolean>;
   error$?: Observable<Error | false>;
 
+  //TABLE
   displayedColumns = ['idx', 'name', 'email', 'phone', 'date'];
 
+  //PAGINATOR
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   dataSource!: MatTableDataSource<IUser[]>;
   results?: any;
+
+  //MAT SORT
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
@@ -28,10 +35,11 @@ export class ListComponent implements OnInit {
 
   fetchUsers() {
     this.users$ = this.userService.getUsers().pipe(
-      tap( data => {
+      tap((data) => {
         this.results = data;
         this.dataSource = new MatTableDataSource(this.results);
         this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       })
     );
 
@@ -53,4 +61,26 @@ export class ListComponent implements OnInit {
       catchError((err) => of(err))
     );
   }
+
+  sortData(sort: Sort) {
+    const data = this.results.slice();
+    if (!sort.active || sort.direction === '') {
+      this.results = data;
+      return;
+    }
+
+    this.results = data.sort((a: any, b: any) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }

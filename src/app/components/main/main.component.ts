64657f6909b39214ac0ea2265580dc9e80435bb9
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-02-04 12:24:12 +0100

    refactor: test

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  private url =
+    'https://angular-material-ba815-default-rtdb.europe-west1.firebasedatabase.app/users/.json';
+  users: any[] = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.fetchUsers();
   }
 
+  fetchUsers() {
+    this.http
+      .get(this.url)
+      .pipe(
+        map((data:any) => Object.keys(data).forEach((key) => this.users.push({id: key, ...data[key]})))
+      )
+      .subscribe();
+  }
 }

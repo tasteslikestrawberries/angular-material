a4e951722de001c774864def19c98da841d17196
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-09-12 10:45:04 +0200

    decorator update

--- src/app/custom-decorators/Logger.ts
+++ src/app/custom-decorators/Logger.ts
@@ -6,20 +6,22 @@
 
 //METHOD DECORATOR
 export function Logger() {
-  return function (targetClass: Object, functionName: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  return function <T extends Object>(targetClass: T, functionName: keyof T, descriptor: PropertyDescriptor): PropertyDescriptor {
     // initial value of the decorated variable/method
     const originalMethod = descriptor.value;
 
     // decorator pattern (wrapping the original method - decorating it with custom functionality)
     // if arrow f was used here, it would take the outer function's this and we need the caller's this
-    const newMethod = function (...args: any[]) {
+    /* It's better to modify the original descriptor than overwriting the current one by returning a new descriptor. 
+    This allows you to use multiple decorators that edit the descriptor without overwriting what another decorator did.*/
+    
+    descriptor.value = function (...args: any[]) {
       console.log('logger: ', functionName);
 
       /*have to call it (not just bind it) because when the decorated method is called it is 
       expected to behave the same as the original method but with the added functionality*/
       return originalMethod.apply(this, args);
     }
-    descriptor.value = newMethod;
 
     //decorators expect descriptor (if they have it) or void as return value
     return descriptor
